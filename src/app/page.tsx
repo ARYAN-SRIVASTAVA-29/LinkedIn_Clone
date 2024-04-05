dc91cccbcd574a022a63ffd285126201edb796a3
Aryan srivastava <mainaryansrivastav@gmail.com>
2024-04-05 23:11:23 +0530

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
+import HeroSection from "@/components/HeroSection";
+import MusicSchoolTestimonials from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+import WhyChooseUs from "@/components/WhyChooseUs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]"></main>
-   <h1 className="text-2xl text-center">Test</h1>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+   
+   <HeroSection/>
+  <FeaturedCourses/>
+  <WhyChooseUs/>
+  <MusicSchoolTestimonials/>
+  <UpcomingWebinars/>
+   </main>
    </>
   );
 }
